f468a07be63be39e271aa016dce63f6c382db7af
trisha-sagar764 <125465931+trisha-sagar764@users.noreply.github.com>
2025-09-30 19:40:10 +0530

    test.py

---
+++ test.py
@@ -0,0 +1,110 @@
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.applications.xception import Xception
+from keras.models import load_model
+from pickle import load
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import argparse
+from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, Dropout
+from tensorflow.keras.layers import add
+from tensorflow.keras.models import Model
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True, help="Image Path")
+args = vars(ap.parse_args())
+img_path = args['image']
+
+def extract_features(filename, model):
+        try:
+            image = Image.open(filename)
+            
+        except:
+            print("ERROR: Couldn't open image! Make sure the image path and extension is correct")
+        image = image.resize((299,299))
+        image = np.array(image)
+        # for images that has 4 channels, we convert them into 3 channels
+        if image.shape[2] == 4: 
+            image = image[..., :3]
+        image = np.expand_dims(image, axis=0)
+        image = image/127.5
+        image = image - 1.0
+        feature = model.predict(image)
+        return feature
+
+def word_for_id(integer, tokenizer):
+ for word, index in tokenizer.word_index.items():
+     if index == integer:
+         return word
+ return None
+
+
+def generate_desc(model, tokenizer, photo, max_length):
+    in_text = 'start'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        pred = model.predict([photo,sequence], verbose=0)
+        pred = np.argmax(pred)
+        word = word_for_id(pred, tokenizer)
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'end':
+            break
+    return in_text
+
+from keras.utils import plot_model
+
+def define_model(vocab_size, max_length):
+
+    # features from the CNN model squeezed from 2048 to 256 nodes
+    inputs1 = Input(shape=(2048,), name='input_1')
+    fe1 = Dropout(0.5)(inputs1)
+    fe2 = Dense(256, activation='relu')(fe1)
+
+    # LSTM sequence model
+    inputs2 = Input(shape=(max_length,), name='input_2')
+    se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+    se2 = Dropout(0.5)(se1)
+    se3 = LSTM(256)(se2)
+
+    # Merging both models
+    decoder1 = add([fe2, se3])
+    decoder2 = Dense(256, activation='relu')(decoder1)
+    outputs = Dense(vocab_size, activation='softmax')(decoder2)
+
+    # tie it together [image, seq] [word]
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+    # summarize model
+    print(model.summary())
+    plot_model(model, to_file='model.png', show_shapes=True)
+
+    return model
+
+#path = 'Flicker8k_Dataset/111537222_07e56d5a30.jpg'
+max_length = 32
+tokenizer = load(open("tokenizer.p","rb"))
+vocab_size = len(tokenizer.word_index) + 1
+
+# First define the model architecture
+model = define_model(vocab_size, max_length)
+# Then load the weights
+model.load_weights('models/model_9.h5')
+xception_model = Xception(include_top=False, pooling="avg")
+
+photo = extract_features(img_path, xception_model)
+img = Image.open(img_path)
+
+description = generate_desc(model, tokenizer, photo, max_length)
+print("\n\n")
+print(description)
+plt.imshow(img)
+
+
+# python test.py --image Flicker8k_Dataset/1859941832_7faf6e5fa9.jpg
+# correct
